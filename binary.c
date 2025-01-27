a438d580bbdcba39f8547f1d461e7cb39fa496fd
Seamus Webb <webbseamus2@gmail.com>
2025-01-27 10:12:18 -0800

    init

---
+++ binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void printBinary16(unsigned int num) {
+    for (int i = 15; i >= 0; i--) {
+        // Extract the i-th bit using a bitwise AND and shift
+        unsigned int bit = (num >> i) & 1;
+        printf("%u", bit);
+    }
+    printf("\n");
+}
+int main() {
+     unsigned int number;
+
+    // Get user input
+    printf("Enter a number (0 to 65535): ");
+    scanf("%u", &number);
+
+    // Check for 16-bit range
+    if (number > 65535) {
+        printf("Error: Number exceeds 16-bit range.\n");
+        return 1;
+    }
+
+    printf("16-bit binary: ");
+    printBinary16(number);
+
+    return 0; 
+}
